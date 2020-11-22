e7850914b894365c53232b70e0642e7da593e597
Éloi Rivard <eloi.rivard@aquilenet.fr>
2020-11-22 19:09:52 +0100

    Several %s are allowed in a ldap filter

--- users_ldap.py
+++ users_ldap.py
@@ -24,3 +24,39 @@ class CompanyLDAP(osv.osv):
         if conf["ldap_tls"]:
             connection.start_tls_s()
         return connection
+
+    def authenticate(self, conf, login, password):
+        """
+        Authenticate a user against the specified LDAP server.
+        In order to prevent an unintended 'unauthenticated authentication',
+        which is an anonymous bind with a valid dn and a blank password,
+        check for empty passwords explicitely (:rfc:`4513#section-6.3.1`)
+
+        :param dict conf: LDAP configuration
+        :param login: username
+        :param password: Password for the LDAP user
+        :return: LDAP entry of authenticated user or False
+        :rtype: dictionary of attributes
+        """
+
+        if not password:
+            return False
+
+        entry = False
+        filter = filter_format(conf['ldap_filter'], [login] * conf['ldap_filter'].count('%s'))
+        try:
+            results = self.query(conf, filter.encode('utf-8'))
+
+            # Get rid of (None, attrs) for searchResultReference replies
+            results = [i for i in results if i[0]]
+            if results and len(results) == 1:
+                dn = results[0][0]
+                conn = self.connect(conf)
+                conn.simple_bind_s(dn, password.encode('utf-8'))
+                conn.unbind()
+                entry = results[0]
+        except ldap.INVALID_CREDENTIALS:
+            return False
+        except ldap.LDAPError, e:
+            _logger.error('An LDAP exception occurred: %s', e)
+        return entry
